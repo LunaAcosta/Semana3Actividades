8df76b4a35c9e9502fc8d7e86c206e2d40715853
Luna Acosta <lunaacosta300@gmail.com>
2021-08-14 16:00:08 -0500

    Tarea De La Semana #3

--- 3.Actividad3ClasePractica/Program.cs
+++ 3.Actividad3ClasePractica/Program.cs
@@ -13,38 +13,66 @@ namespace _3.Actividad3ClasePractica
             string[] genero = new string[10];
             string[] estudiantes = new string[10];
 
+            int femenino = 0;
+            int masculino = 0;
+
+            double[] edad = new double [10];
+         
+            double prom = 0;
+
+
+
             for (int i = 0; i < estudiantes.Length; i++)
             {
                 Console.Write("INGRESE EL NOMBRE DEL ALUMNO {0}:  ", i);
                 estudiantes[i] = Console.ReadLine();
 
-                Console.Write("SELECCIONE SU GENERO F / M {0} :  ", i);
+                Console.Write("SELECCIONE SU GENERO f / m {0} :  ", i);
                 genero[i] = Console.ReadLine();
+                
+
+                Console.Write("INGRESE LA EDAD DEL ALUMNO {0}: ", i);
+                edad[i] = double.Parse(Console.ReadLine());
                 Console.WriteLine();
+
+
             }
             Console.WriteLine();
             Console.WriteLine("DATOS DE LOS ESTUDIANTES: ");
             Console.WriteLine();
+
+
             // Array.Sort(estudiantes);
 
 
             for (int i = 0; i < estudiantes.Length; i++)
-
-
             {
-            
-                Console.WriteLine("SU NOMBRE ES : {0} | DEL GENERO : {1}",estudiantes[i], genero[i]);
-               
-                //if (genero[i] == "F")
-                
-                    
-                
 
-               // Console.WriteLine(genero[i]);
+                Console.WriteLine("SU NOMBRE ES : {0} | DEL GENERO : {1}", estudiantes[i], genero[i]);
+
+                if (genero[i] == "f")
+                {
+                    femenino += 1;
+                }
+
+                if (genero[i] == "m")
+                {
+                    masculino += 1;
+                }
+
+                 prom = edad.Average();
             }
+
+            Console.WriteLine();
+                Console.WriteLine("LA CANTIDAD DE ESTUDIANTES DEL GENERO FEMENINO ES: {0}", femenino);
+                Console.WriteLine("LA CANTIDAD DE ESTUDIANTES DEL GENERO MASCULINO ES: {0}", masculino);
+                Console.WriteLine("EL PROMEDIO DE LAS EDADES DE LOS ESTUDIANTES ES DE: {0}", prom);
+
+
             Console.ReadKey();
 
+            
         }
-
     }
-}
+ }
+
