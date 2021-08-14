b3fe49728cc8b5c0a6cfff64e7e7c22cd15731b9
Luna Acosta <lunaacosta300@gmail.com>
2021-08-13 22:50:28 -0500

    Agregar archivos de proyecto.

---
+++ 2.Actividad 2 ClasePractica/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Actividad_2_ClasePractica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] estudiantes = new string[10];
+            
+            for (int i = 0; i < estudiantes.Length; i++ )
+            {
+                Console.Write("ESCRIBA EL NOMBRE DEL ESTUDIANTE  {0}:  ", i);
+
+                estudiantes[i] = Console.ReadLine();
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("DATOS DE LOS ESTUDIANTES : ");
+
+           // Array.Sort(estudiantes);
+
+            for (int i = 0; i < estudiantes.Length; i++ )
+            {
+                Console.WriteLine(estudiantes[i]);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 3.Actividad3ClasePractica/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Actividad3ClasePractica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] genero = new string[10];
+            string[] estudiantes = new string[10];
+
+            for (int i = 0; i < estudiantes.Length; i++)
+            {
+                Console.Write("INGRESE EL NOMBRE DEL ALUMNO {0}:  ", i);
+                estudiantes[i] = Console.ReadLine();
+
+                Console.Write("SELECCIONE SU GENERO F / M {0} :  ", i);
+                genero[i] = Console.ReadLine();
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("DATOS DE LOS ESTUDIANTES: ");
+            Console.WriteLine();
+            // Array.Sort(estudiantes);
+
+
+            for (int i = 0; i < estudiantes.Length; i++)
+
+
+            {
+            
+                Console.WriteLine("SU NOMBRE ES : {0} | DEL GENERO : {1}",estudiantes[i], genero[i]);
+               
+                //if (genero[i] == "F")
+                
+                    
+                
+
+               // Console.WriteLine(genero[i]);
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
